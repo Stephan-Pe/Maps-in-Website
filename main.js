b58655579bf9e39cc3f7f21c28c6274d2890f9a7
Stephan-Pe <root@DESKTOP-AIDFMFU.localdomain>
2019-11-19 16:04:47 +0100

    set location

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function initMap(){
-    var location = {lat: 46.781260, lng: 9.543160};
+    var location = {lat: 20.798363, lng: -156.331924};
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12,
+        zoom: 8,
         center: location
     });
     var marker = new google.maps.Marker({
